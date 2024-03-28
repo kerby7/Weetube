dbeaed1fdfb3ea527bcd354a3a79684b9db0f938
Shounak Chakraborty <shounakchakraborty7@gmail.com>
2024-03-28 20:05:18 +0530

    Project done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import { Stack } from '@mui/material';
-import {Link} from "react-router-dom";
-import {logo} from "../Utils/constants";
-import { Search } from '@mui/icons-material';
-import SearchBar from './SearchBar';
+import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
+import logo from "../Components/yttt.png";
+import SearchBar from "./SearchBar";
 
 function Navbar() {
   return (
-    <Stack direction= "row" alignItems = "center" p = {2} sx = {{position: "sticky", background: "#000", top : 0, justifyContent: "space-between"}}>
-      <Link to = "/" style={{display: "flex", alignItems: "center"}}>
-        <img src= {logo} alt='logo' height={45} />
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
       </Link>
       <SearchBar />
     </Stack>
-  )
+  );
 }
 
 export default Navbar;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,15 +1,26 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Paper, IconButton } from "@mui/material";
-import { Search, SearchIcon } from "@mui/icons-material";
+import SearchIcon from "@mui/icons-material/Search";
 
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
-function SearchBar() {
+  const onhandleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+
+      setSearchTerm("");
+    }
+  };
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={onhandleSubmit}
       sx={{
         borderRadius: 20,
         border: "1px solid #e3e3e3",
@@ -20,15 +31,20 @@ function SearchBar() {
     >
       <input
         className="search-bar"
-        placeholder="Search here"
-        value=""
-        onChange={(e) => {}}
-      ></input>
-      <IconButton type="submit" sx={{p: '10px', color: 'red'}}>
-        <Search />
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)
+        }
+      />
+      <IconButton
+        type="submit"
+        sx={{ p: "10px", color: "red" }}
+        aria-label="search"
+      >
+        <SearchIcon />
       </IconButton>
     </Paper>
   );
-}
+};
 
 export default SearchBar;
